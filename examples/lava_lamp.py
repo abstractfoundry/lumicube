6fe4e4aba8b9ca29838237db6411d1f99704128e
JensKula <jens.kula@arcor.de>
2021-10-04 18:54:04 +0200

    Update lava_lamp.py
    Fixed to be executed in lumikube simulator

--- examples/lava_lamp.py
+++ examples/lava_lamp.py
@@ -3,6 +3,7 @@
 # Simplex noise is way of generating random numbers across a 2D, 3D or 4D space
 # whether each point is similar to its neighbour. So you get a randomised landscape with peaks and troughs
 from opensimplex import OpenSimplex
+import time
 
 noise = OpenSimplex()
 scale = 0.10 # How spread out are the features
@@ -17,6 +18,6 @@ while True:
                     # Noise returns a number between -1 and 1
                     hue = noise.noise4d(x * scale, y * scale, z * scale, t * speed) / 2 + 0.65
                     colours[(x,y,z)] = hsv_colour(hue, 1, 1)
-    display.set_3d(colours, True)
+    display.set_3d(colours)
     time.sleep(1/20)
     t += 1
